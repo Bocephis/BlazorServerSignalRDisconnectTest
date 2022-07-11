e49585ad34e8a14b34e5cfb521baba96d8b40973
Mark Coffman <markcoffman@gmail.com>
2022-07-11 10:23:52 -0400

    Add project files.

---
+++ BlazorServerSignalRDisconnectTest/Hubs/RoomHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorServerSignalRDisconnectTest.Hubs
+{
+    public class RoomHub : Hub
+    {
+        private static int ConnectedUsers = 0;
+
+        public async override Task OnConnectedAsync()
+        {
+            ConnectedUsers++;
+
+            Console.WriteLine($"Connecting: {Context.ConnectionId}");
+
+            await Clients.All.SendAsync("UserCountChanged", ConnectedUsers);
+            await base.OnConnectedAsync();
+        }
+        public async override Task OnDisconnectedAsync(Exception? exception)
+        {
+            ConnectedUsers--;
+
+            Console.WriteLine($"Disconnecting: {Context.ConnectionId}");
+
+            await Clients.All.SendAsync("UserCountChanged", ConnectedUsers);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
+
